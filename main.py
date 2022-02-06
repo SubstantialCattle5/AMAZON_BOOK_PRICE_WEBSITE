d1b397360fab860aff00cb072dc0a19ee857c459
Nilay Nath Sharan <92233871+SubstantialCattle5@users.noreply.github.com>
2022-02-06 06:35:01 +0530

    Integrating flask without cool stuff

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request
+import a_scrape
+import messaging
 
 app = Flask(__name__)
 link = str()
 price = 0
 
 
+def amazon(url_link: str):
+    amazon_data = a_scrape.Amazon_Scrape(url=url_link)
+    print(amazon_data.bookname)
+
+
 @app.route('/', methods=["POST", "GET"])
 def main_pg():
     if request.method == 'POST':
-        global link, price
         link, price = request.form['link_text'], request.form['value']
+        amazon(url_link=f'{link}')
         return render_template('index.html')
     else:
         return render_template('index.html')
