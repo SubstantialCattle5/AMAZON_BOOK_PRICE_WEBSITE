06b9778dbcb4d655acaf94735d15aa3856d1c1d0
Nilay Nath Sharan <92233871+SubstantialCattle5@users.noreply.github.com>
2022-02-07 12:17:08 +0530

    finishing the flask part of the project

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request
+import json
+
+from flask import Flask, render_template, request, redirect, url_for
 import a_scrape
 from amazon_link_regex import Amazon_Link_Regex
 
 app = Flask(__name__)
 link = str()
 price = 0
-book_names, max_book_prices = [], []
+book_names, max_book_prices = list(), list()
+book_ = dict()
+book_links = list()
 
 
 # Extracting the book names from the link
@@ -15,31 +19,59 @@ def book_names_list(links):
     return book_names_regex.book_name
 
 
+# Amazon JSON dump
+
+def amazon_json_dump():
+    data = dict()
+    with open('amazon_book_links.json', 'w') as filehandle:
+        for i, j in zip(book_links, max_book_prices):
+            data[j] = i
+
+        json.dump(data, filehandle, indent=4)
+
+
 @app.route('/', methods=["POST", "GET"])
 def main_pg():
-    global book_names, max_book_prices
+    global book_names, max_book_prices, book_links
     # POST is for buttons save and submit
     if request.method == 'POST':
         # Save Button , passing value should not be zero
         if request.form.get('save') == 'Save' and len(request.form['link_text']) != 0:
-            link, price = request.form['link_text'], request.form['value']
-            book_names = book_names + [book_names_list(link)]  # Creating the list
-            max_book_prices = max_book_prices + [int(price) * 10]
-
+            book_names = book_names + [book_names_list(request.form['link_text'])]  # Creating the list
+            max_book_prices = max_book_prices + [int(request.form['value']) * 10]  # Creating price list
+            book_links.append(request.form['link_text'])
             return render_template('index.html', titles=book_names)
 
-        # Submit button
         elif request.form.get('Submit') == 'submit_':
-            return render_template('index.html')
+            return redirect(url_for('confirmation_pg'))
 
-
-        # Save button but length is not equal to zero
+        # Save button but length is equal to zero
         else:
+            book_names, book_links = [], []
             return render_template('index.html')
 
     else:  # clearing the page
-        book_names = []
+        book_names, book_links = [], []
         return render_template('index.html')
+
+
+@app.route('/confirmation', methods=['POST', 'GET'])
+def confirmation_pg():
+    if len(book_names) == 0:
+        books = ['No books saved yet!']
+    else:
+        books = book_names
+    # Confirm Button
+    if request.method == 'POST':
+        if request.form.get('save') == 'Save':
+            if len(book_links) == 0 and len(max_book_prices) == 0:
+                return render_template('connfirmation.html', books=['ERROR! NO BOOKS SAVED'])
+            else:
+                amazon_json_dump()
+                return render_template('confirmation.html', books=['Saved!'])
+
+    elif request.method == 'GET':
+        return render_template('confirmation.html', books=book_names)
 
 
 if __name__ == '__main__':
